72a8e900d108cade16b26c7d71ad3b9d99aadca6
SUMANTH BANAKAR <sumanthbanakar@SUMANTHs-MBP.lan>
2021-09-16 23:18:37 -0400

    GitCommands

---
+++ HelloWorld/src/helloworld/HelloSangavi.java
@@ -0,0 +1,8 @@
+package helloworld;
+
+public class HelloSangavi {
+    public static void main(String[] args) {
+        System.out.print("HelloSangavi");
+        System.out.print("HelloSumanth");
+    }
+}
--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -5,6 +5,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         //Print the words Hello World on the screen    
         System.out.println("Hello World");
+        System.out.println("Sumanth");
+        System.out.println("Sangavi");
+        System.out.println("Hello");
     }
-    
+
 }
